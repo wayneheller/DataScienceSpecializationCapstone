65b243c1f1752d1dab0d2ecef94855a1b13ebb74
Wayne Heller <waynegheller@gmail.com>
2017-08-27 10:04:04 -0700

    Added code to remove the non-ASCII characters from the sample

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Coursera | Johns Hopkins University | Capstone Project | Data Science Specializa
 This course is the final project in a series of 10 courses in this specialization and is meant to demonstrate skills acquired over the 9 previous course and new learning -- Natural Language Processing (NLP)
 
 *"This course will start with the basics, analyzing a large corpus of text documents to discover the structure in the data and how words are put together. It will cover cleaning and analyzing text data, then building and sampling from a predictive text model. Finally, you will use the knowledge you gained in data products to build a predictive text product you can show off to your family, friends, and potential employers."*
+
+## Sources
++ Profanity List - https://www.freewebheaders.com/full-list-of-bad-words-banned-by-google/  Was looking to download directly from Google but have not yet found that link.
+
+
--- gathercleandata.R
+++ gathercleandata.R
@@ -1,19 +1,36 @@
-# Data Science Capstone Project
-# August 2017
-#
-#
-# Read and sample text files
-# Row counts were determined by opening file in a text editor
+################################################################################
+# Coursera|Johns Hopkins Data Science Science Specializaiton|Capstone Project  #
+# October - December 2017                                                      #
+# Wayne Heller                                                                 #
+#                                                                              #
+# To create the sample data file:                                              #
+# > createSampleData(datadir)                                                  #
+#                                                                              #
+# To open the corpus:                                                          #
+# > myCorpus <- openCorpus(sampledir)                                          #
+#                                                                              #
+# To remove profanity, puncuation, whitespace, etc.                            #
+# > myCorpus <- cleanCorpus(myCorpus, metadatadir)                             #
+#                                                                              #
+################################################################################
 
 library(tm)
 
+# path variable names
 datadir <- file.path(getwd(), "data")
 sampledir <- file.path(datadir, "sampleddata")
 metadatadir <- file.path(datadir, "metadata")
 
+# helper function to convert an ascii code to its character
+#used by cleanCorpus()
 chr <- function(n) { rawToChar(as.raw(n)) }
 
+# creates a sample dataset from a file
+# as inputs: the full path to the file, total number of rows in the file, 
+# and the sample size
+# expressed as a decimal between 0.0 and 1.0
 sampleText <- function (filename, totalrows, sampleSize) {  
+        # for reproducibility
         set.seed(12345)
         
         # generate line numbers of the file to include in the sample
@@ -31,16 +48,21 @@ sampleText <- function (filename, totalrows, sampleSize) {
                 row <- row + 1
                         
                 if(row %in% lineNumber) {
-                        sample <- list(sample, oneLine)   
+                        # Add 1 line to the sample row set, stripping out the non-ASCII characters
+                        sample <- list(sample, iconv(oneLine, from = "latin1", to="ASCII", sub=""))   
                 }
         }
         close(conn)
         return(unlist(sample))
 }
 
+# Read and sample text files
+# Row counts were determined by opening file in a text editor
+# input is the full path to the directory containing the original corpus
+# output: a Corpus object
 createSampleData <- function(datadir) {
         sample.size <- 0.00001
-        samplefilename <- "sampledata.txt"
+        samplefilename <- paste0("sampledata_samplesize_", as.character(sample.size), ".txt")
         
         file.news <- "en_US.news.txt"
         file.news.rows <- 1010242
@@ -61,17 +83,19 @@ createSampleData <- function(datadir) {
         
         
         # Remove existing sample file if it exists
-        if(file.exists(file.path(datadir, samplefilename))) {
-                file.remove(file.path(datadir, samplefilename))
+        if(file.exists(file.path(sampledir, samplefilename))) {
+                file.remove(file.path(sampledir, samplefilename))
         }
         
         # Create new sample file
-        conn <- file(file.path(datadir, "sampleddate", samplefilename), open = "w", encoding = "UTF-8" )
+        conn <- file(file.path(sampledir, samplefilename), open = "w", encoding = "UTF-8" )
         write(samples, conn)
         close(conn)
 }
 
 # open sample data as a corpus
+# input: full path to the directory containing the sampled data file
+# output: a Corpus object
 openCorpus <- function(sampleDir) {
         #txt <- system.file(sampleDir, "txt", package = "tm")
         #print(txt)
@@ -83,11 +107,14 @@ openCorpus <- function(sampleDir) {
 
 # This function removes non-breaking spaces ascii character 160
 # The trimws() function does not handle these
+# Used by cleanCorpus() in the preparation of the banned words file
 removenbsp <- function(x) {
         return(gsub(chr(160), "", x, fixed = TRUE))
 }
 
-removePattern <- content_transformer(function(x, pattern) {return (gsub(pattern, "", x, fixed = FALSE))})
+# This function is a generic content transformer used by function cleanCorpus to replace special characters with ""
+# Currently not used by cleanCorpus but may need it later, so retaining it.
+removePattern <- content_transformer(function(x, pattern) gsub(pattern, "", x, fixed = TRUE))
 
 # Removes Profanity, Whitespace, and Punctuation
 cleanCorpus <- function(myCorpus, metadataDir) {
@@ -98,9 +125,11 @@ cleanCorpus <- function(myCorpus, metadataDir) {
         # This file contains non-breaking whitespace that needs to be removed
         myCorpus <- tm_map(myCorpus, removeWords, sapply(BandWords[, 1], removenbsp))
         
+        # This step is no longer needed since we remove all non-ASCII characters
+        # when creating this file
         # Step 2: Remove special characters of the form <U+9999>"
         # gsub("<U\\+\\d+>", "", x, fixed=FALSE)
-        myCorpus <- tm_map(myCorpus, removePattern, "<U") # not working yet
+        #myCorpus <- tm_map(myCorpus, removePattern, "<") 
         
         # Step 3: Remove punctuation
         myCorpus <- tm_map(myCorpus, removePunctuation)
